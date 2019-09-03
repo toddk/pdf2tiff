cb4fdf254e8b74eb219629dbafd50677185ef279
Todd Krokowski <toddkrokowski@gmail.com>
2019-09-03 08:33:56 -0400

    Added logging via winston.

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const fs = require('fs');
-const Busboy = require('busboy');
-const dotenv = require('dotenv');
-const util = require('util');
-const path = require('path');
-const cors = require('cors');
+const express = require('express'),
+    fs = require('fs'),
+    Busboy = require('busboy'),
+    dotenv = require('dotenv'),
+    path = require('path'),
+    cors = require('cors'),
+    winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service'},
+    transports: [
+        new winston.transports.File( {filename: 'error.log', level: 'error'}),
+        new winston.transports.File( {filename: 'combined.log'})
+    ]
+});
+
 const exec = require('child_process').exec;
 const app = express();
 
@@ -12,11 +23,11 @@ let indir = '/usr/data/in/';
 let outdir = '/usr/data/out/';
 
 exec(`sh scripts/translate.sh ${indir} ${outdir}`, (error, stdout, stderr) => {
-    console.log("Starting watcher script...");
-    console.log(stdout);
-    console.log(stderr);
+    logger.info("Starting watcher script...");
+    logger.info(stdout);
+    logger.warn(stderr);
     if (error) {
-        console.log(`exec error: ${error}`);
+        logger.error(`exec error: ${error}`);
     }
 });
 
@@ -33,28 +44,25 @@ app.route('/convert').post( (req, res) => {
     var uploadedFile = '';
 
     busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated) => {
-        console.log(`'${fieldname}' : '${val}'`);
+        logger.info(`'${fieldname}' : '${val}'`);
         formData.set(fieldname, val);
     });
 
     busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
         uploadedFile = filename;
         var saveTo = path.join(indir, filename);
-        console.log(`Uploading to ${saveTo}`);
+        logger.info(`Uploading to ${saveTo}`);
         file.pipe(fs.createWriteStream(saveTo));
     });
 
     busboy.on('finish', () => {
-        console.log('Upload complete');
+        logger.info('Upload complete');
         var metadata = path.join(outdir, `${uploadedFile}.txt`);
         fs.writeFileSync(metadata, `{'email': '${formData.get('email')}', 'name': '${formData.get('name')}'}` , 'utf-8', (err) => {
-            if (err) console.log(err);
-            console.log("Metadata written to file for use after conversion");
+            if (err) logger.error(err);
+            logger.info("Metadata written to file for use after conversion");
         });
         
-
-        console.log(`Name: ${formData.get('name')}`);
-        console.log(`Email: ${formData.get('email')}`);
         res.writeHead(200, { 'Connection' : 'close' });
         res.end('Upload complete!');
     });
